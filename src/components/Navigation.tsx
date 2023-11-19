fd6c8b9cdd6f638ff8f90464f8b05c930f0eedf2
yared-dev <yaredper@gmail.com>
2023-11-18 19:38:46 -0500

    translate

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,61 +2,76 @@ import { useState } from "react";
 import { MdMenu } from "react-icons/md";
 import { MdClose } from "react-icons/md";
 
-
-
-
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+  const menuIcon = (
+    <MdMenu
+      className="menu"
+      size="30px"
+      onClick={() => setMenuOpen(!menuOpen)}
+    />
+  );
+  const closeIcon = (
+    <MdClose
+      className="menu"
+      size="30px"
+      onClick={() => setMenuOpen(!menuOpen)}
+    />
+  );
 
-    const menuIcon = <MdMenu className="menu" size="30px" onClick={()=>setMenuOpen(!menuOpen)}/>;
-    const closeIcon = <MdClose className="menu" size="30px" onClick={()=>setMenuOpen(!menuOpen)}/>;
+  const MenuList = [
+    {
+      tittle: "MENU 1",
+      childs: [],
+      clicked: true,
+      redirection: "/",
+    },
+    {
+      tittle: "MENU 2",
+      childs: [],
+      clicked: true,
+      redirection: "/",
+    },
+    {
+      tittle: "MENU 3",
+      childs: [],
+      clicked: true,
+      redirection: "/",
+    },
+    {
+      tittle: "MENU 4",
+      childs: [],
+      clicked: true,
+      redirection: "/",
+    },
+    {
+      tittle: "MENU 5",
+      childs: [],
+      clicked: true,
+      redirection: "/",
+    },
+  ];
+  const listaElementos = MenuList.map((elemento, index) => (
+    <li key={index}>
+      <a href="#menu1" onClick={() => setMenuOpen(false)}>
+        {elemento.tittle}
+      </a>
+    </li>
+  ));
+  return (
+    <div className="header">
+      <nav id="navbar" className="container">
+        {!menuOpen ? menuIcon : closeIcon}
+        <div className="logo">
+          <a href="#page-top">
+            <img src="img/logo.svg" alt="Logo LLC" />
+          </a>{" "}
+        </div>
 
-
-
-    
-    return (
-        <div className="header">
-            <nav id="navbar" className="container">
-            {!menuOpen? menuIcon:closeIcon}                
-            <div className="logo">
-                <a href="#page-top">
-                    <img src="img/logo.svg"  alt="Logo LLC"/>
-                </a>{" "}
-            </div>
-           
-            <ul className={`items ${menuOpen ? "open" : ""}`}>
-                    <li>
-                        <a href="#menu1" onClick={() => setMenuOpen(false)}>
-                            MENU 1
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#about">
-                            MENU 2
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#services">
-                            MENU 3
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#portfolio">
-                            MENU 4
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#testimonials">
-                            MENU 5
-                        </a>
-                    </li>                    
-                </ul>
-            
-            </nav>
+        <ul className={`items ${menuOpen ? "open" : ""}`}>{listaElementos}</ul>
+      </nav>
     </div>
-           
-        
-    )
-}
+  );
+};
 export default Navigation;
